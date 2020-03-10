f019ac47915f418849c6d3b1c8b1c9dd51ff0f0b
Matthew <mshin1995@gmail.com>
2020-03-09 22:40:03 -0700

    GET

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const google = require('googleapis')
+const {google} = require('googleapis')
 const sheets = google.sheets('v4')
 const spreadsheetId = '1dG_Bxm2UfnpMCvtbscuY-NnvNnOQ7izLzPvFstXyPiM'
 const credentials = require('./credentials.json')
@@ -7,3 +7,24 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.use(bodyParser.json())
+
+const auth = new google.auth.JWT (
+    credentials.client_email,
+    null,
+    credentials.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets'],
+    null
+)
+
+google.options({auth})
+
+sheets.spreadsheets.values.get({
+    spreadsheetId,
+    range: 'Sheet1',  
+}, (err, resp) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(resp.data.values)
+    }
+});
