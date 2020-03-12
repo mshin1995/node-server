7a65e58c359dda62fe6c506780df9919e704693b
Matthew <mshin1995@gmail.com>
2020-03-11 22:31:24 -0700

    comments

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const {google} = require('googleapis');
 const sheets = google.sheets('v4');
-const spreadsheetId = '1dG_Bxm2UfnpMCvtbscuY-NnvNnOQ7izLzPvFstXyPiM';
+const { GoogleSpreadsheet }= require('google-spreadsheet');
 const credentials = require('./credentials.json');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
 
 app.use(bodyParser.json())
 
@@ -69,3 +68,18 @@ app.delete('/data/:key', (req, res) => {
 
 app.listen(3000)
 
+// async function accessSpreadsheet() {
+//     const doc = new GoogleSpreadsheet('1dG_Bxm2UfnpMCvtbscuY-NnvNnOQ7izLzPvFstXyPiM');
+//     await doc.useServiceAccountAuth((credentials));
+//     await doc.loadInfo();
+//     const sheet = doc.sheetsByIndex[0];
+//     const rows = await sheet.getRows();
+//     console.log(rows)
+// }
+
+// Tried to use the google-spreadsheet npm, but decided not to. One of the main issues I ran into while working on this assignment
+// was trying to figure out how to effectively use Google Sheets as a database. I didn't know how to set unique identifiers 
+// for each key-value pair using a Google Sheet which is needed to delete specific pairs. I did however know how to delete entire rows, so
+// I set up individual key-value pairs on each row using the row's number as a unique id. That is why the solution I have implemented 
+// above has a constraint of the key having to be the same number as the row it is on, since I was only able to use the row number as 
+// the unique identifier for each key-value pair.
